bc6541a25328a2eb65144249b88a8d2f1d5a67a4
Abcefggh <75517235+Abcefggh@users.noreply.github.com>
2021-10-09 16:21:58 +0530

    Add files via upload

---
+++ SearchScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, View, StyleSheet} from 'react-native';
+import AppHeader from '../components/AppHeader'
+
+export default class TransactionScreen extends React.Component {
+  render() {
+    return (
+      <View>
+      <AppHeader />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={styles.text}>Instagram</Text>
+      </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    padding: 250,
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
